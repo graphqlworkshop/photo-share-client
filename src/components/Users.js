7f85a3695b5e1812f8606320c8f718119a027ffc
MoonTahoe <alex@moonhighway.com>
2018-09-17 16:29:18 -0700

    Incorporates UI for Users

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,25 +1,12 @@
 import React from 'react'
-import { Query, Mutation } from 'react-apollo'
-import { ROOT_QUERY, ADD_TEST_USER } from '../operations'
+import { Query } from 'react-apollo'
+import { ROOT_QUERY } from '../operations'
+import { UserList } from './ui'
 
 const Users = () =>
     <Query query={ROOT_QUERY} fetchPolicy="cache-and-network">
-        {({ data, loading }) => loading ?
-            <p>loading...</p> :
-            <div>
-                <p>total Users: {data.totalUsers}</p>
-                <Mutation mutation={ADD_TEST_USER} refetchQueries={[{query: ROOT_QUERY }]}>
-                    {addTestUser => <button onClick={addTestUser}>Add Test User</button>}
-                </Mutation>
-                <ul>
-                    {data.allUsers.map(user => 
-                        <li key={user.githubLogin}> 
-                            <img src={user.avatar} width={48} height={48} alt="" />
-                            {user.name}
-                        </li>
-                    )}
-                </ul>
-            </div>
+        {({ data, loading }) => 
+            <UserList users={data.allUsers} loading={loading} />
         }
     </Query>
 
