0fc88b648f3449f5622079f5a7625ac04524a15e
MoonTahoe <alex@moonhighway.com>
2018-09-17 11:59:20 -0700

    Send a Test Query

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import { render } from 'react-dom'
-import ApolloClient from 'apollo-boost'
+import ApolloClient, { gql } from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
 
-const client = new ApolloClient({ uri: 'http://localhost:4000/graphql' })
+const client = new ApolloClient({ uri: 'http://localhost:4000/graphql '})
+
+const TOTALS = gql`
+    query totals {
+        totalPhotos
+        totalUsers
+    }
+`
+
+client.query({ query: TOTALS })
+    .then(console.log)
 
 render(
   <ApolloProvider client={client}>
