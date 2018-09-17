2a5ba777816b4fd991dde4ab5c69d975c046b5b1
MoonTahoe <alex@moonhighway.com>
2018-09-17 13:44:14 -0700

    React Configuration

--- src/components/App.js
+++ src/components/App.js
@@ -1,55 +1,15 @@
 import React from 'react'
-import { gql } from 'apollo-boost'
-import { Query, Mutation } from 'react-apollo'
+import { BrowserRouter } from 'react-router-dom'
+import AuthorizedUser from './AuthorizedUser'
+import Users from './Users'
 
-const ALL_USERS = gql`
-    query users {
-        totalUsers
-        allUsers {
-            githubLogin
-            avatar
-            name
-        }
-    }
-`
-
-const ADD_TEST_USER = gql`
-    mutation addTestUser {
-        githubAuth(code: "TEST") {
-            token
-            user {  
-                githubLogin
-                avatar
-                name
-            }
-        }
-    }
-`
-
-const Users = () =>
-    <Query query={ALL_USERS} pollInterval={1000}>
-        {({ data, loading }) => loading ?
-            <p>loading...</p> :
-            <div>
-                <p>total Users: {data.totalUsers}</p>
-                <Mutation mutation={ADD_TEST_USER}>
-                    {addTestUser => <button onClick={addTestUser}>Add Test User</button>}
-                </Mutation>
-                <ul>
-                    {data.allUsers.map(user => 
-                        <li key={user.githubLogin}> 
-                            <img src={user.avatar} width={48} height={48} alt="" />
-                            {user.name}
-                        </li>
-                    )}
-                </ul>
-            </div>
-        }
-    </Query>
-
-
-
-const App = () => <Users />
-
+const App = () =>
+    <BrowserRouter>
+        <div>
+            <AuthorizedUser />
+            <Users />
+        </div>
+       
+    </BrowserRouter>
 
 export default App
