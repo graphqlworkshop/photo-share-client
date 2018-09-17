ed4df0568f327eab29f0cc9481db6ffaea618ffb
MoonTahoe <alex@moonhighway.com>
2018-09-17 12:53:56 -0700

    Display all users

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,40 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
-import AuthorizedUser from './AuthorizedUser'
-import Users from './Users'
+import { gql } from 'apollo-boost'
+import { Query } from 'react-apollo'
 
-const App = () =>
-    <BrowserRouter>
-        <div>
-            <AuthorizedUser />
-            <Users />
-        </div>
-       
-    </BrowserRouter>
+const ALL_USERS = gql`
+    query users {
+        totalUsers
+        allUsers {
+            githubLogin
+            avatar
+            name
+        }
+    }
+`
+
+const Users = () =>
+    <Query query={ALL_USERS}>
+        {({ data, loading }) => loading ?
+            <p>loading...</p> :
+            <div>
+                <p>total Users: {data.totalUsers}</p>
+                <ul>
+                    {data.allUsers.map(user => 
+                        <li key={user.githubLogin}> 
+                            <img src={user.avatar} width={48} height={48} alt="" />
+                            {user.name}
+                        </li>
+                    )}
+                </ul>
+            </div>
+            
+        }
+    </Query>
+
+
+
+const App = () => <Users />
+
 
 export default App
