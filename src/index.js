903d54837eb8a4c7d07c1f2b44cd8a4cf9f1cff5
MoonTahoe <alex@moonhighway.com>
2018-09-17 15:06:51 -0700

    Adds a websocket client

--- src/index.js
+++ src/index.js
@@ -1,20 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
-import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
 import App from './components/App'
-
-const client = new ApolloClient({ 
-  uri: 'http://localhost:4000/graphql',
-  request: operation => {
-    operation.setContext(context => ({
-        headers: {
-            ...context.headers,
-            authorization: localStorage.getItem('token')
-        }
-    }))
-  }
-})
+import client from './photo-share-client'
 
 render(
   <ApolloProvider client={client}>
