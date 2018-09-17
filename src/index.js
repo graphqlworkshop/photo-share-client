00df7daa1c884c139725f84b3791cd811e788524
MoonTahoe <alex@moonhighway.com>
2018-09-17 12:53:56 -0700

    Display all users

--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
 import React from 'react'
 import { render } from 'react-dom'
-import ApolloClient, { gql } from 'apollo-boost'
+import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
+import App from './components/App'
 
 const client = new ApolloClient({ uri: 'http://localhost:4000/graphql '})
 
-const TOTALS = gql`
-    query totals {
-        totalPhotos
-        totalUsers
-    }
-`
-
-client.query({ query: TOTALS })
-    .then(console.log)
-
 render(
   <ApolloProvider client={client}>
-    <h1>Hello World</h1>
+    <App />
   </ApolloProvider>,
   document.getElementById('root')
 )  
