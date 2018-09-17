246a8250d7b8851ff95c9c47cd900470f45a4f78
MoonTahoe <alex@moonhighway.com>
2018-09-17 13:44:14 -0700

    React Configuration

--- src/components/AuthorizedUser.js
+++ src/components/AuthorizedUser.js
@@ -1,64 +1,11 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
-import { Query, Mutation } from 'react-apollo'
-import { GITHUB_AUTH, ROOT_QUERY } from '../operations'
 
-const CurrentUser = ({ name, avatar }) =>
-    <div>
-        <img src={avatar} width={48} height={48} alt="" />
-        <h1>{name}</h1>
-    </div>
-
-const Me = ({ onRequestCode=f=>f, signingIn=false }) =>
-    <Query query={ROOT_QUERY}>
-        {({ loading, data }) => data.me ?
-            <CurrentUser {...data.me} /> :
-            loading ?
-                <p>loading... </p> :
-                <button onClick={onRequestCode}
-                    disabled={signingIn}>
-                    Sign In with Github
-                </button>
-        }
-    </Query>
-
-class AuthorizedUser extends Component {
-
-    state = { signingIn: false }
-    
-    requestCode = () => {
-       const clientID = process.env.REACT_APP_GITHUB_CLIENT_ID
-       window.location = `https://github.com/login/oauth/authorize?client_id=${clientID}&scope=user`
+ class AuthorizedUser extends Component {
+     render() {
+        console.log('withRouter adds props: ', this.props)
+        return <button>Sign In with Github</button>
     }
-
-    authorizationComplete = (cache, { data }) => {
-        localStorage.setItem('token', data.githubAuth.token)
-        const { history } = this.props
-        this.setState({ signingIn: false })
-        history.replace('/')
-    }
-
-    componentDidMount() {
-       if (window.location.search.match(/code=/)) {
-           this.setState({ signingIn: true })
-           const code = window.location.search.replace("?code=", "")
-           this.authorize({ variables: {code} })
-       }
-    }
-
-    render() {
-        return (
-            <Mutation mutation={GITHUB_AUTH} update={this.authorizationComplete} refetchQueries={[{ query: ROOT_QUERY }]}>
-                {authorize => {
-                    this.authorize = authorize
-                    return (
-                        <Me onRequestCode={this.requestCode} signingIn={this.state.signingIn} />
-                    )
-                }}
-            </Mutation>
-        )
-      }
-
-}
+ }
 
  export default withRouter(AuthorizedUser) 
