2d835acea3f84831fa2e6b06746ba74e84c7396f
MoonTahoe <alex@moonhighway.com>
2018-09-17 14:32:58 -0700

    Display the current user

--- src/index.js
+++ src/index.js
@@ -4,7 +4,17 @@ import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
 import App from './components/App'
 
-const client = new ApolloClient({ uri: 'http://localhost:4000/graphql '})
+const client = new ApolloClient({ 
+  uri: 'http://localhost:4000/graphql',
+  request: operation => {
+    operation.setContext(context => ({
+        headers: {
+            ...context.headers,
+            authorization: localStorage.getItem('token')
+        }
+    }))
+  }
+})
 
 render(
   <ApolloProvider client={client}>
