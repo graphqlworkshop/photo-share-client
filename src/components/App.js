552b139b486e2b4bf905e107ab7c4cf1d494eb34
MoonTahoe <alex@moonhighway.com>
2018-09-17 15:28:13 -0700

    Adds Listen for User Subscription

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import { withApollo } from 'react-apollo'
 import AuthorizedUser from './AuthorizedUser'
 import Users from './Users'
+import { ROOT_QUERY, LISTEN_FOR_USERS } from '../operations'
 
-const App = () =>
-    <BrowserRouter>
-        <div>
-            <AuthorizedUser />
-            <Users />
-        </div>
-    </BrowserRouter>
+class App extends Component {
 
-export default App
+    componentDidMount() {
+        let { client } = this.props
+        this.listenForUsers = client
+            .subscribe({ query: LISTEN_FOR_USERS })
+            .subscribe(({ data:{ newUser } }) => {
+                const data = client.readQuery({ query: ROOT_QUERY })
+                data.totalUsers += 1
+                data.allUsers = [
+                    ...data.allUsers,
+                    newUser
+                ]
+                client.writeQuery({ query: ROOT_QUERY, data })
+            }) 
+    }
+
+    componentWillUnmount() {
+        this.listenForUsers.unsubscribe()
+    }
+
+    render() {
+        return (
+            <BrowserRouter>
+                <div>
+                    <AuthorizedUser />
+                    <Users />
+                </div>
+            </BrowserRouter>
+        )
+    }
+
+}
+
+export default withApollo(App)
