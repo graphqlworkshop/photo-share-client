cd6107838e721b8813446396b0db5965ff89d773
MoonTahoe <alex@moonhighway.com>
2018-09-17 13:44:14 -0700

    React Configuration

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,12 +1,49 @@
 import React from 'react'
-import { Query } from 'react-apollo'
-import { ROOT_QUERY } from '../operations'
-import { UserList } from './ui'
+import { gql } from 'apollo-boost'
+import { Query, Mutation } from 'react-apollo'
+
+const ALL_USERS = gql`
+    query users {
+        totalUsers
+        allUsers {
+            githubLogin
+            avatar
+            name
+        }
+    }
+`
+
+const ADD_TEST_USER = gql`
+    mutation addTestUser {
+        githubAuth(code: "TEST") {
+            token
+            user {  
+                githubLogin
+                avatar
+                name
+            }
+        }
+    }
+`
 
 const Users = () =>
-    <Query query={ROOT_QUERY} fetchPolicy="cache-and-network">
-        {({ data, loading }) => 
-            <UserList users={data.allUsers} loading={loading} />
+    <Query query={ALL_USERS} pollInterval={1000}>
+        {({ data, loading }) => loading ?
+            <p>loading...</p> :
+            <div>
+                <p>total Users: {data.totalUsers}</p>
+                <Mutation mutation={ADD_TEST_USER}>
+                    {addTestUser => <button onClick={addTestUser}>Add Test User</button>}
+                </Mutation>
+                <ul>
+                    {data.allUsers.map(user => 
+                        <li key={user.githubLogin}> 
+                            <img src={user.avatar} width={48} height={48} alt="" />
+                            {user.name}
+                        </li>
+                    )}
+                </ul>
+            </div>
         }
     </Query>
 
